1fd110d601556544bd5e4f69555a70c6984195f9
gjnzsu <30156758@qq.com>
2020-07-19 16:38:09 +0800

    0719: python import test

---
+++ PythonTestStep02.py
@@ -0,0 +1,10 @@
+#!Import test
+
+import sys
+print('=============Python import mode')
+x=sys.argv
+print(x[0])
+#for i in sys.argv:
+#    print('Test Entry')
+#    print(i)
+print('=============Python import mode end')
